72d0219fec762ac0c7f854a4777964f7396b729e
Polemos <28989218+polemion@users.noreply.github.com>
2018-10-28 22:07:07 +0200

    Initial Polemos fork commits.

--- Mopy/mash/mlox/loader.py
+++ Mopy/mash/mlox/loader.py
@@ -1,54 +1,58 @@
-import cPickle
+# -*- coding: utf-8 -*-
+
+# Wrye Mash Polemos fork GPL License and Copyright Notice ==============================
+#
+# This file is part of Wrye Mash Polemos fork.
+#
+# Wrye Mash 2018 Polemos fork Copyright (C) 2017-2018 Polemos
+# * based on code by Yacoby copyright (C) 2011-2016 Wrye Mash Fork Python version
+# * based on code by Melchor copyright (C) 2009-2011 Wrye Mash WMSA
+# * based on code by Wrye copyright (C) 2005-2009 Wrye Mash
+# License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+#
+#  Copyright on the original code 2005-2009 Wrye
+#  Copyright on any non trivial modifications or substantial additions 2009-2011 Melchor
+#  Copyright on any non trivial modifications or substantial additions 2011-2016 Yacoby
+#  Copyright on any non trivial modifications or substantial additions 2017-2018 Polemos
+#
+# ======================================================================================
+
+# Original Wrye Mash License and Copyright Notice ======================================
+#
+#  Wrye Mash is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  Wrye Bolt is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Mash; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  Wrye Mash copyright (C) 2005, 2006, 2007, 2008, 2009 Wrye
+#
+# ========================================================================================
+
+
 import os
-import fnmatch
-import imp
+import scandir  # Polemos
 
-def findMlox(start):
-    """ 
-    Attempts to find mlox in the give path. It avoids serching Data Files
-    """
-    for root, dirnames, filenames in os.walk(start):
-        try:
-            dirnames.remove('Data Files')
-        except ValueError:
-            pass
-        try:
-            dirnames.remove('Installers')
-        except ValueError:
-            pass
 
-        for filename in fnmatch.filter(filenames, 'mlox.py'):
-            return root
-    return None
+def findMlox(mloxexe, exclusions, target):  # Polemos
+    """Attempts to find mlox in the given path. It avoids searching Data Files, Installers etc."""
+    exclusions = set([os.path.basename(x).lower() for x in exclusions])
+    for root, dirs, files in scandir.walk(target, topdown=True):
+        [dirs.remove(x) for x in list(dirs) if x.lower() in exclusions]
+        x = [os.path.join(root, file) for file in files if file == mloxexe]
+        if x: return x[0]
 
-def mloxFromCfg():
-    try:
-        pk = open('startup.settings.pkl', 'rb');
-        mloxPath = cPickle.load(pk)
-        pk.close()
-        if os.path.exists( os.path.join(mloxPath, 'mlox.py') ):
-            return mloxPath
-    except (IOError, EOFError):
-        pass
-    return None
 
-def saveMloxCfg(path):
-    pk = open('startup.settings.pkl', 'wb');
-    mloxPath = cPickle.dump(path, pk)
-    pk.close()
-
-def importMlox():
-    wd = os.getcwd()
-    mloxPath = mloxFromCfg() or findMlox(os.path.dirname(wd))
-
-    if mloxPath:
-        #ugly hack to get around some mlox data loading issues
-        os.chdir(mloxPath)
-        mlox = imp.load_source('mlox', os.path.join(mloxPath, 'mlox.py'))
-        os.chdir(wd)
-
-        saveMloxCfg(mloxPath)
-        return mlox
-    else:
-        from . import fakemlox
-        return fakemlox
+def Mlox_The_Path(mloxexe, exclusions, target):  # Polemos
+    """Returns mlox path."""
+    if os.path.dirname(target) == target: return ''  # Avoid root pls...
+    try: return findMlox(mloxexe, exclusions, target)
+    except: return ''
