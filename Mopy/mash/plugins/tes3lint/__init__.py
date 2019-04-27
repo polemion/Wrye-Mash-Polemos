7564e5d975f81249320287e7f5bed48adfab2e6e
Polemos <28989218+polemion@users.noreply.github.com>
2019-04-27 17:24:07 +0300

    Wrye Mash 2019 - v100 pre-release Beta Updates

---
+++ Mopy/mash/plugins/tes3lint/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Wrye Mash Polemos fork GPL License and Copyright Notice ==============================
+#
+# This file is part of Wrye Mash Polemos fork.
+#
+# Wrye Mash 2018 Polemos fork Copyright (C) 2017-2019 Polemos
+# * based on code by Yacoby copyright (C) 2011-2016 Wrye Mash Fork Python version
+# * based on code by Melchor copyright (C) 2009-2011 Wrye Mash WMSA
+# * based on code by Wrye copyright (C) 2005-2009 Wrye Mash
+# License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+#
+#  Copyright on the original code 2005-2009 Wrye
+#  Copyright on any non trivial modifications or substantial additions 2009-2011 Melchor
+#  Copyright on any non trivial modifications or substantial additions 2011-2016 Yacoby
+#  Copyright on any non trivial modifications or substantial additions 2017-2019 Polemos
+#
+# ======================================================================================
+
+
+# Plugin for Wrye Mash Polemos fork ======================================================
+#
+# TES3lint implementation, Copyright (C) 2018-, Polemos
+#
+# License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher.
+#
+# ===========================================================================================
