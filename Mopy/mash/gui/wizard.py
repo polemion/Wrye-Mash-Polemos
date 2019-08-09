a6a6b26a57f6cdfa5367b8aa5e0161dc76bcd7cd
Polemion <28989218+polemion@users.noreply.github.com>
2019-08-09 19:59:56 +0300

    Updated License.

--- Mopy/mash/gui/wizard.py
+++ Mopy/mash/gui/wizard.py
@@ -4,7 +4,7 @@
 #
 # This file is part of Wrye Mash Polemos fork.
 #
-# Wrye Mash 2018 Polemos fork Copyright (C) 2017-2019 Polemos
+# Wrye Mash, Polemos fork Copyright (C) 2017-2019 Polemos
 # * based on code by Yacoby copyright (C) 2011-2016 Wrye Mash Fork Python version
 # * based on code by Melchor copyright (C) 2009-2011 Wrye Mash WMSA
 # * based on code by Wrye copyright (C) 2005-2009 Wrye Mash
